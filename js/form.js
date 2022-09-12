adc1140ffef4bf4ecc532819e16f013088a6c4ee
blessyBBiju-Github <110586255+BlessyBBiju-github@users.noreply.github.com>
2022-09-12 15:47:30 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,48 +1,138 @@
-<script>    
-        function validate() {    
-            var fname = document.reg_form.fname;    
-            var lname = document.reg_form.lname;    
-            var address = document.reg_form.address;    
-            var gender = document.reg_form.gender;    
-            var email = document.reg_form.email;    
-            var mobile = document.reg_form.mobile;    
-            var course = document.reg_form.course;    
-    
-            if (fname.value.length <= 0) {    
-                alert("Name is required");    
-                fname.focus();    
-                return false;    
-            }    
-            if (lname.value.length <= 0) {    
-                alert("Last Name is required");    
-                lname.focus();    
-                return false;    
-            }    
-            if (address.value.length <= 0) {    
-                alert("Address is required");    
-                address.focus();    
-                return false;    
-            }    
-            if (gender.value.length <= 0) {    
-                alert("Gender is required");    
-                gender.focus();    
-                return false;    
-            }    
-            if (email.value.length <= 0) {    
-                alert("Email Id is required");    
-                email.focus();    
-                return false;    
-            }    
-            if (mobile.value.length <= 0) {    
-                alert("Mobile number is required");    
-                mobile.focus();    
-                return false;    
-            }    
-            if (course.value == "select course") {    
-                alert("Course is required");    
-                course.focus();    
-                return false;    
-            }    
-            return false;    
-        }    
-    </script>    
+
+
+ * Run init() after the page is loaded
+window.onload = init;
+
+ * Initialization
+ function init() {
+   document.getElementById("formTest").onsubmit = validateForm;
+   document.getElementById("btnReset").onclick = clearForm;
+   document.getElementById("txtName").focus();
+}
+ 
+
+function validateForm(theForm) {
+   with(theForm) {
+     return (isNotEmpty(txtName, "Please enter your name!", elmNameError)
+           && isNumeric(txtZipcode, "Please enter a 5-digit zip code!", elmZipcodeError)
+           && isLengthMinMax(txtZipcode, 6, 6, "Please enter a 6-digit zip code!", elmZipcodeError)
+           && isSelected(selCountry, "Please make a selection!", elmCountryError)
+           && isChecked("gender", "Please check a gender!", elmGenderError)
+           && isChecked("color", "Please check a color!", elmColorError)
+           && isNumeric(txtPhone, "Please enter a valid phone number!", elmPhoneError)
+           && isValidEmail(txtEmail, "Enter a valid email!", elmEmailError)
+           && isValidPassword(txtPassword, "Password shall be 6-8 characters!", elmPasswordError)
+           && verifyPassword(txtPassword, txtPWVerified, "Different from new password!",
+                 elmPWVerifiedError)
+      );
+   }
+}
+ 
+function postValidate(isValid, errMsg, errElm, inputElm) {
+   if (!isValid) {
+     if (errElm !== undefined && errElm !== null
+            && errMsg !== undefined && errMsg !== null) {
+         errElm.innerHTML = errMsg;
+      }
+      
+      if (inputElm !== undefined && inputElm !== null) {
+         inputElm.classList.add("errorBox");  // Add class for styling
+         inputElm.focus();
+      }
+   } else {
+     if (errElm !== undefined && errElm !== null) {
+         errElm.innerHTML = "";
+      }
+      if (inputElm !== undefined && inputElm !== null) {
+         inputElm.classList.remove("errorBox");
+      }
+   }
+}
+ function isNotEmpty(inputElm, errMsg, errElm) {
+   var isValid = (inputElm.value.trim() !== "");
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+
+function isNumeric(inputElm, errMsg, errElm) {
+   var isValid = (inputElm.value.trim().match(/^\d+$/) !== null);
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+
+function isAlphabetic(inputElm, errMsg, errElm) {
+   var isValid = (inputElm.value.trim().match(/^[a-zA-Z]+$/) !== null) ;
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+function isAlphanumeric(inputElm, errMsg, errElm) {
+   var isValid = (inputElm.value.trim().match(/^[0-9a-zA-Z]+$/) !== null);
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+function isLengthMinMax(inputElm, minLength, maxLength, errMsg, errElm) {
+   var inputValue = inputElm.value.trim();
+   var isValid = (inputValue.length >= minLength) && (inputValue.length <= maxLength);
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+function isValidEmail(inputElm, errMsg, errElm) {
+   var isValid = (inputElm.value.trim().match(
+         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) !== null);
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+
+function isSelected(selectElm, errMsg, errElm) {
+   var isValid = (selectElm.value !== "");   // value in selected <option>
+   postValidate(isValid, errMsg, errElm, selectElm);
+   return isValid;
+}
+ 
+
+function isChecked(inputName, errMsg, errElm) {
+   var elms = document.getElementsByName(inputName);
+   var isChecked = false;
+   for (var i = 0; i < elms.length; ++i) {
+      if (elms[i].checked) {
+         isChecked = true;
+         break;
+      }
+   }
+   postValidate(isChecked, errMsg, errElm, null);  // no focus element
+   return isChecked;
+}
+ 
+function isValidPassword(inputElm, errMsg, errElm) {
+   var isValid = (inputElm.value.trim().match(/^\w{6,8}$/) !== null);
+   postValidate(isValid, errMsg, errElm, inputElm);
+   return isValid;
+}
+ 
+
+function verifyPassword(pwElm, pwVerifiedElm, errMsg, errElm) {
+   var isTheSame = (pwElm.value === pwVerifiedElm.value);
+   postValidate(isTheSame, errMsg, errElm, pwVerifiedElm);
+   return isTheSame;
+}
+ 
+
+function clearForm() {
+   // Remove class "errorBox" from input elements
+   var elms = document.querySelectorAll('.errorBox');  // class
+   for (var i = 0; i < elms.length; i++) {
+      elms[i].classList.remove("errorBox");
+   }
+ 
+   elms = document.querySelectorAll('[id$="Error"]');  // id ends with Error
+   for (var i = 0; i < elms.length; i++) {
+      elms[i].innerHTML = "";
+   }
+ document.getElementById("txtName").focus();
+}
